8608928bae174e494911c68dc8c20f06f23ba247
EstebanMakoatoOwO <estebansoft2003@gmail.com>
2026-05-04 13:33:38 -0600

    chore: se agrego la dependencia de mercado pago y  modificaciones de seguridad, aplication properties

--- src/main/java/com/andromedaastroshop/crudfullstack/crud_fullstack/config/SecurityConfig.java
+++ src/main/java/com/andromedaastroshop/crudfullstack/crud_fullstack/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
                         .requestMatchers("/images/**").permitAll()
-                        .requestMatchers("/api/v1/orders/webhook/**").permitAll()
+                        .requestMatchers("/api/v1/payments/webhook").permitAll()
                         .requestMatchers(org.springframework.http.HttpMethod.POST, "/api/v1/orders").permitAll()
                         .anyRequest().authenticated()
                 )
